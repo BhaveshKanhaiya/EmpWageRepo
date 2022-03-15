30684732930f4b3daedf4e88d845bf49be3cef08
BhaveshKanhaiya <bhaveshkanhaiya@gmail.com>
2022-03-15 10:27:35 +0530

    MODIFIED: Employee Working 100 Hrs Calculation

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+
+
+    public class Constants {
+        final static int WAGE_PER_HR = 20;
+        final static int PART_TIME_HR = 4;
+        final static int FULL_TIME_HR = 8;
+        final static int WORKING_DAYS = 20;
+        final static int IS_ABSENT = 0;
+        final static int IS_PRESENT = 1;
+        final static int IS_PART_TIME = 0;
+        final static int IS_FULL_TIME = 1;
+        final static int TOTAL_WORKING_HRS = 100;
+    }
